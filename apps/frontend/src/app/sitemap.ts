c48c4444a49dc2022da89ea2f739813025f35994
Gustavo <gustavolimamartin2002@gmail.com>
2025-10-19 15:54:45 -0300

    Update sitemap.ts
    feat: nova lógica e tratamento de erros;

--- apps/frontend/src/app/sitemap.ts
+++ apps/frontend/src/app/sitemap.ts
@@ -2,14 +2,37 @@ import { services } from '@/data/services';
 import { ProjectModel } from '@/models/projectModel';
 import type { MetadataRoute } from 'next';
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/projects`);
-  const projects: ProjectModel[] = await response.json();
+interface ApiResponse {
+  data: ProjectModel[];
+  currentPage: number;
+  totalPages: number;
+  totalProjects: number;
+}
 
-  const projectUrls: MetadataRoute.Sitemap = projects.map((project) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/projeto/${project._id}`,
-    lastModified: project.date || new Date(),
-  }));
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  let projectUrls: MetadataRoute.Sitemap = [];
+
+  try {
+    const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/projects`;
+    const response = await fetch(apiUrl, { cache: 'no-store' });
+
+    if(response.ok) {
+      const projects:ApiResponse = await response.json();
+
+      if (Array.isArray(projects.data)) {
+        projectUrls = projects.data.map((project) => ({
+          url: `${process.env.NEXT_PUBLIC_BASE_URL}/projeto/${project._id}`,
+          lastModified: project.date ?? new Date(),
+        }));
+      } else {
+        console.warn('API retornou dados inválidos (propriedade "data" ausente ou não é array).');
+      }
+    } else {
+      console.error(`Falha ao buscar projetos para o sitemap. Status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error('Exceção capturada durante o fetch do sitemap:', error);
+  }
 
   const serviceUrls: MetadataRoute.Sitemap = services.map((service) => ({
     url: `${process.env.NEXT_PUBLIC_BASE_URL}/servico/${service.id}`,
